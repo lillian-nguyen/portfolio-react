917a729f91a43880280c78fbf2b464fba38ac239
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-04-27 23:09:43 -0500

    removed borders from hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,26 +5,26 @@ export default function Hero(){
         <>
         <Nav/>
         <section className="hero">
-        <section id="photoContainer">
-        <section id="hero-content">
+        {/* <section id="photoContainer"> */}
+        {/* <section id="hero-content">
             <img id="selfie" src="src/assets/beach.jpg" alt="Lillian Nguyen selfie"/>
             <section className="header-text">
                 <h1>Lillian Nguyen</h1>
                 <p>software engineer based in Houston, TX</p>
             </section>
-        </section>
+        </section> */}
         
-        {/* <section className="header-text">
-            <img id="selfie" src="src/assets/beach.jpg" alt="Lillian Nguyen selfie"/>
+        <section className="header-text">
+            {/* <img id="selfie" src="src/assets/beach.jpg" alt="Lillian Nguyen selfie"/> */}
             <h1>Lillian Nguyen</h1>
             <p>software engineer based in Houston, TX</p>
-        </section> */}
+        </section>
 
         <div className="scrollArrowContainer bounce">
             <span></span>
         </div>
         </section>
-    </section>
+    {/* </section> */}
         </>
     )
 }
