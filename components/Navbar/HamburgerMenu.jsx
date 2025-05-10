8f651aa0ea5778748a710da9e3b9d4eb979b23a7
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-05-10 08:46:02 -0500

    feat: mobile links page

--- components/Navbar/HamburgerMenu.jsx
+++ components/Navbar/HamburgerMenu.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const HamburgerMenu = () => {
+const HamburgerMenu = ({ className = "" }) => {
     return (
-    <button className="hamburger-menu"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAI5JREFUWEftlkEOgCAMBHd/Ji9TXyY/q6lRIwGvWw7lyGWXASYlgheD85EF5iJgZoviTZCsT85LwMwOAJICAHaSm5f4FvCNVUEAQHkozPUGRKdvYpJAEugIKGT0JyKlB8JF1Bfwz6nG36g4QkJZIAnMReD+gj4VKVa4iCrJ0l2BQkRX6GgoVXAfZeQ8kAROiicuISwfoZAAAAAASUVORK5CYII=" alt="navigation button"/></button>
+    <button className={`hamburger-menu ${className}`}><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAI5JREFUWEftlkEOgCAMBHd/Ji9TXyY/q6lRIwGvWw7lyGWXASYlgheD85EF5iJgZoviTZCsT85LwMwOAJICAHaSm5f4FvCNVUEAQHkozPUGRKdvYpJAEugIKGT0JyKlB8JF1Bfwz6nG36g4QkJZIAnMReD+gj4VKVa4iCrJ0l2BQkRX6GgoVXAfZeQ8kAROiicuISwfoZAAAAAASUVORK5CYII=" alt="navigation button"/></button>
     );
 }
 
--- components/Navbar/MobileMenu.jsx
+++ components/Navbar/MobileMenu.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import HamburgerMenu from './HamburgerMenu';
+import Line from '../Line';
+import Square from '../Square';
 
 const MobileMenu = () => {
     return(
-        <div className="w-screen h-screen bg-[#445A29]">
-            <h1 className="m-0">mobile</h1>
+        <div className="fixed inset-0 bg-[#445A29] flex flex-col justify-center items-center">
+            <HamburgerMenu className="mobilemenu-hamburger"/>
+            <div className="flex flex-col items-center mobile-links">
+                <a>HOME</a>
+                <a>ABOUT</a>
+                <a>PROJECTS</a>
+                <a>CONTACT</a>
+            </div>
+            <Line className="line1"/>
+            <Line className="line2"/>
+            <Square/>
+            <img src="src/assets/flowerbg-removed.png" className="flowers"></img>
+            <h3 className="mobile-initials">LN</h3>
         </div>
     )
 }
