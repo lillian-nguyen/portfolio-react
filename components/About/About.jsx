4e478ea994bd29778faddb42da2f3f25d54ca3ba
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-05-20 13:49:42 -0500

    feat: add more to about section

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -7,33 +7,44 @@ return(
         <section className={styles.topRow}>
             <Button
             style={{backgroundColor: '#445A29'}}
-            className="aboutNavButton"
+            className={styles.aboutNavButton}
             />
             <h2 className={styles.aboutHeader}>ABOUT ME</h2>
         </section>
        
+       <section className={styles.aboutContainer}>
+
         <section className={styles.themeContainer}>
-            <h4>I CREATE JOY AND PURPOSE THROUGH:</h4>
+            <h4>CREATING MY SPACE THROUGH:</h4>
+
             <div className={styles.themeOptions}>
-                <button>PROGRAMMING</button>
-                <button>SIDE QUESTS</button>
-                <button>EXPLORING</button>
+                <p className={styles.activeAboutText}>PROGRAMMING</p>
+
+                <div className={styles.inactiveAboutButtons}>
+                    <button>SIDE QUESTS</button>
+                    <button>EXPLORING</button>
+                </div>
             </div>
         </section>
 
-        <section>
+        <div className={styles.polaroidWrapper}>
+            <img className={styles.smiski} src="src/assets/smiski_researching.png" alt="smiski" />
             <img className={styles.aboutImg} src="https://images.unsplash.com/photo-1656680632373-e2aec264296b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-            <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2dzZWp0YzlnemV2bTVtdG55Mjkxa3Vrd3h1dHE5bWtkbDV3OWtxNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/t8nUMbu4NHNFQUKTq5/giphy.gif" alt="" />
-            <section className={styles.imgCurrently}>
+            <img className={styles.polaroid} src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2dzZWp0YzlnemV2bTVtdG55Mjkxa3Vrd3h1dHE5bWtkbDV3OWtxNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/t8nUMbu4NHNFQUKTq5/giphy.gif" alt="" />
+        </div>
+            
+            <section className={styles.currently}>
                 <h4>CURRENTLY</h4>
                     <img src="#" alt="" />
                     <span>Coding @ Gridiron Survivor</span>
                     <img src="#" alt="" />
                     <span>Develping an app to connect long distance friends</span>
+                    <span>Counting down days until CYC Conference</span>
                 </section>
-        </section>
         
-        <section>bio blurb</section>
+        
+        {/* <section>bio blurb</section> */}
+    </section>
     </section>
     );
 }
