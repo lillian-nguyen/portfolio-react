4306b20a10a767d3032827d0e49e081a771dc99b
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-05-21 18:46:54 -0500

    fix: move assets to public folder for proper rendering on deployment

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -9,9 +9,9 @@ const Projects = () => {
             <div className={styles.projectsContainer}>
             <h4>VENTURE WEAVERS</h4>
             <IndividualProject
-            imgSrc="src/assets/ventureweavers.mp4"
+            imgSrc="/ventureweavers.mp4"
             // projectName="Venture Weavers"
-            projectBio="A modern, responsive site for Venture Weavers, a life science consulting group, complete with tailored content and branding."
+            projectBio="A clean, responsive site for Venture Weavers, a life science consulting group, complete with tailored content and branding."
             tech= {['HTML', 'CSS', 'JavaScript']}
             />
             </div>
