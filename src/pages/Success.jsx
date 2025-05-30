1e2103ff54599893126ccfab8ebc07a921880309
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-05-30 00:04:19 -0500

    delete

--- src/pages/Success.jsx
+++
@@ -1,8 +0,0 @@
-const success = () => (
-  <div style={{ padding: "4rem", textAlign: "center" }}>
-    <h1>Thank You! 🎉</h1>
-    <p>Your message has been sent successfully. I'll be in touch soon!</p>
-  </div>
-);
-
-export default success;
